deb18dbb6786f6ceb63dc4284f8d398a27901954
Jared Males <jaredmales@gmail.com>
2018-12-27 21:26:08 -0700

    added license blurb

--- milk0Server.hpp
+++ milk0Server.hpp
@@ -6,6 +6,25 @@
   * - 2018 created by JRM
   */
 
+//***********************************************************************//
+// Copyright 2018 Jared R. Males (jaredmales@gmail.com)
+//
+// This file is part of milk0mq.
+//
+// milk0mq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// milk0mq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with milk0mq.  If not, see <http://www.gnu.org/licenses/>.
+//***********************************************************************//
+
 #ifndef milk0Server_hpp
 #define milk0Server_hpp
 
